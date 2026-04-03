2c70d4dc2087971d6563b5097f09d7e4ba83c7a4
maple <2361017046@qq.com>
2026-04-03 18:14:17 +0800

    chore: 初始化handy box app配置

---
+++ apps/handy-box-mobile/app/_layout.tsx
@@ -0,0 +1,37 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { TamaguiProvider } from 'tamagui';
+import { tamaguiConfig } from '../tamagui.config';
+
+import 'react-native-reanimated';
+import '../css/global.css';
+
+import { useColorScheme } from '@/hooks/use-color-scheme';
+
+export const unstable_settings = {
+  anchor: '(tabs)',
+};
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="modal"
+            options={{ presentation: 'modal', title: 'Modal' }}
+          />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </TamaguiProvider>
+  );
+}
---
+++ apps/handy-box-mobile/tamagui.config.ts
@@ -0,0 +1,18 @@
+import { defaultConfig } from '@tamagui/config/v5';
+import { animations as animationsCSS } from '@tamagui/config/v5-css';
+import { animations as animationsReanimated } from '@tamagui/config/v5-reanimated';
+import { createTamagui, isWeb } from 'tamagui';
+
+export const tamaguiConfig = createTamagui({
+  ...defaultConfig,
+  animations: isWeb ? animationsCSS : animationsReanimated,
+});
+
+export default tamaguiConfig;
+
+export type Conf = typeof tamaguiConfig;
+
+declare module 'tamagui' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface TamaguiCustomConfig extends Conf {}
+}
